05d4108f6c866de18b95a21ac14a391e6f49b36c
DanyloNovikov <dnovikov@varteq.com>
2021-11-08 19:01:48 +0200

    Added serilog logging to telegram

--- PriceObserver/Program.cs
+++ PriceObserver/Program.cs
@@ -1,9 +1,12 @@
 using System.IO;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using PriceObserver.Data;
 using PriceObserver.Data.Seed;
+using Serilog;
+using TelegramSink;
 
 namespace PriceObserver
 {
@@ -14,6 +17,8 @@ namespace PriceObserver
             var host = CreateHostBuilder(args).Build();
 
             SeedData(host);
+
+            InitializeLogger();
             
             host.Run();
         }
@@ -21,6 +26,7 @@ namespace PriceObserver
         private static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseSerilog()
                 .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
 
         private static void SeedData(IHost host)
@@ -30,6 +36,22 @@ namespace PriceObserver
             var context = services.GetService<ApplicationDbContext>();
                 
             DbSeeder.Seed(context);
+        }
+        
+        private static void InitializeLogger()
+        {
+            var configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build();
+            
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(configuration)
+                .Enrich.FromLogContext()
+                .MinimumLevel.Verbose()
+                .WriteTo.TeleSink(
+                    "1073477516:AAG7lyhYOzDvKBz8Rp1BUVBjYBbxl-uZbAQ",
+                    "382190306")
+                .CreateLogger();
         }
     }
 }
