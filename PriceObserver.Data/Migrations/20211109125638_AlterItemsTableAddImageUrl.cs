89ce398b406a1e50519ccf90c262226910e2ab7c
DanyloNovikov <dnovikov@varteq.com>
2021-11-09 16:14:47 +0200

    Migration with adding image url to item

---
+++ PriceObserver.Data/Migrations/20211109125638_AlterItemsTableAddImageUrl.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PriceObserver.Data.Migrations
+{
+    public partial class AlterItemsTableAddImageUrl : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ImageUrl",
+                table: "Items",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageUrl",
+                table: "Items");
+        }
+    }
+}
