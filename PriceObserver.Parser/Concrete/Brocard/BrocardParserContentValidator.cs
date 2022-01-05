a13c9e3e8ece4f229c846bdbd8582a7d99705778
DanyloNovikov <dnovikov@varteq.com>
2022-01-05 17:59:59 +0200

    Added validation for brocard

--- PriceObserver.Parser/Concrete/Brocard/BrocardParserContentValidator.cs
+++ PriceObserver.Parser/Concrete/Brocard/BrocardParserContentValidator.cs
@@ -1,4 +1,5 @@
-﻿using AngleSharp.Html.Dom;
+﻿using AngleSharp.Dom;
+using AngleSharp.Html.Dom;
 using PriceObserver.Data.InMemory.Models.Enums;
 using PriceObserver.Parser.Base;
 
@@ -11,7 +12,17 @@ public class BrocardParserContentValidator : ParserProviderContentValidatorBase
     protected override bool IsPriceExists(IHtmlDocument document)
     {
         const string selector = ".price-format > .price";
-            
-        return document.QuerySelector(selector) is not null;
+        
+        var span = document.QuerySelector<IHtmlSpanElement>(selector);
+        if (span == null)
+            return false;
+        
+        var spanText = span.Text();
+
+        var price = spanText.Replace(" ", string.Empty);
+        price = price[..price.IndexOf(',')];
+        
+        var parsedPrice = int.Parse(price);
+        return parsedPrice > 0;
     }
 }
