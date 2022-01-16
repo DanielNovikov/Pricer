d1718b640dcc18eadb4c1d92c505ac65a97fc567
DanyloNovikov <dnovikov@varteq.com>
2022-01-16 22:23:44 +0200

    Added error handling in background parser

--- PriceObserver.Background/JobServices/Concrete/ItemsPriceService.cs
+++ PriceObserver.Background/JobServices/Concrete/ItemsPriceService.cs
@@ -1,6 +1,6 @@
 ﻿using System;
-using System.Resources;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
 using PriceObserver.Background.JobServices.Abstract;
 using PriceObserver.Data.InMemory.Models.Enums;
 using PriceObserver.Data.Models;
@@ -8,7 +8,6 @@ using PriceObserver.Data.Repositories.Abstract;
 using PriceObserver.Data.Service.Abstract;
 using PriceObserver.Parser.Abstract;
 using PriceObserver.Parser.Models;
-using PriceObserver.Telegram.Abstract;
 
 namespace PriceObserver.Background.JobServices.Concrete;
 
@@ -20,6 +19,7 @@ public class ItemsPriceService : IItemsPriceService
     private readonly IItemParseResultRepository _parseResultRepository;
     private readonly IItemParseResultService _parseResultService;
     private readonly IItemRemovalService _itemRemovalService;
+    private readonly ILogger _logger;
 
     public ItemsPriceService(
         IItemRepository itemRepository,
@@ -27,7 +27,8 @@ public class ItemsPriceService : IItemsPriceService
         IItemPriceChanger itemPriceChanger,
         IItemParseResultRepository parseResultRepository,
         IItemParseResultService parseResultService,
-        IItemRemovalService itemRemovalService)
+        IItemRemovalService itemRemovalService,
+        ILogger<ItemsPriceService> logger)
     {
         _itemRepository = itemRepository;
         _parser = parser;
@@ -35,6 +36,7 @@ public class ItemsPriceService : IItemsPriceService
         _parseResultRepository = parseResultRepository;
         _parseResultService = parseResultService;
         _itemRemovalService = itemRemovalService;
+        _logger = logger;
     }
 
     public async Task Observe()
@@ -43,15 +45,35 @@ public class ItemsPriceService : IItemsPriceService
 
         foreach (var item in items)
         {
-            var parsedItemResult = await _parser.Parse(item.Url, item.ShopKey);
-
-            if (!parsedItemResult.IsSuccess)
+            try
             {
-                await DeleteItem(item, parsedItemResult.Error);
-                return;
-            }
+                var parsedItemResult = await _parser.Parse(item.Url, item.ShopKey);
 
-            await ObservePrice(item, parsedItemResult.Result);
+                if (!parsedItemResult.IsSuccess)
+                {
+                    await DeleteItem(item, parsedItemResult.Error);
+                    return;
+                }
+
+                await ObservePrice(item, parsedItemResult.Result);
+            }
+            catch (Exception ex)
+            {
+                const string templateLogMessage = @"Background parser threw an exception
+Item: {0} (Id: {1})
+Item url: {2}
+Message: {3}
+InnerException: {4}";
+
+                _logger.LogError(
+                    ex, 
+                    templateLogMessage,
+                    item.Title,
+                    item.Id,
+                    item.Url,
+                    ex.Message, 
+                    ex.InnerException);
+            }
         }
     }
 
